2007b463cc2d84d57b45c339e2a8e193270e2476
Aryan03112002 <abag1265@gmail.com>
2023-06-19 21:04:00 +0530

    up

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple"> Aryan Barsaiyan </span>
+            from <span className="purple"> Auraiya(U.P.), India.</span>
+            <br/>I am a Full Stack Developer and a Competitive programmer.
+            <br/>
+            <br />I am a junior year pursuing BTech in Computer Science and Engineering in Madan Mohan Malaviya University of Technology, Gorakhpur.
+            
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games.
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writing Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
